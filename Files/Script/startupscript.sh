3203fca60c60166276ad74ceb2a6fe61be938762
Koen-Hertmans <61520912+Koen-Hertmans@users.noreply.github.com>
2020-11-12 18:55:32 +0100

    Update startupscript.sh

--- Files/Script/startupscript.sh
+++ Files/Script/startupscript.sh
@@ -14,7 +14,7 @@ MYSQL_SCRIPT
 
 wget https://download.owncloud.org/community/owncloud-complete-20200731.tar.bz2
 tar -xvf owncloud-complete-20200731.tar.bz2
-mv owncloud /var/www/html/
+sudo mv owncloud /var/www/html/
 sudo chown -R www-data:www-data /var/www/
 
 sudo systemctl unmask hostapd
@@ -22,10 +22,10 @@ sudo systemctl enable hostapd
 sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
 sudo rfkill unblock wlan
 
-cp ../Config/config.php /var/www/html/owncloud
-cp ../Config/dhcpcd.conf /etc/dhcpcd.conf
+sudo cp ../Config/config.php /var/www/html/owncloud
+sudo cp ../Config/dhcpcd.conf /etc/dhcpcd.conf
 sudo cp ../Config/dnsmasq.conf /etc/dnsmasq.conf
-cp ../Config/hostapd.conf /etc/hostapd/hostapd.conf
+sudo cp ../Config/hostapd.conf /etc/hostapd/hostapd.conf
 cp .bashrc /home/pi
 
 sudo reboot
