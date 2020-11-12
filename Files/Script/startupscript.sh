b3228e094963f5ab1da9d29726775a303a919dab
Koen-Hertmans <61520912+Koen-Hertmans@users.noreply.github.com>
2020-11-12 18:05:17 +0100

    Update startupscript.sh

--- Files/Script/startupscript.sh
+++ Files/Script/startupscript.sh
@@ -8,16 +8,23 @@ sudo mysql -u root -p <<MYSQL_SCRIPT
 CREATE DATABASE VOGEL;
 CREATE USER 'pi'@'localhost' IDENTIFIED BY 'rootroot';
 GRANT ALL PRIVILEGES ON VOGEL.* TO 'pi'@'localhost';
-FLUSH PRIVILeGES;
+FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
 wget https://download.owncloud.org/community/owncloud-complete-20200731.tar.bz2
 tar -xvf owncloud-complete-20200731.tar.bz2
 mv owncloud /var/www/html/
 sudo chown -R www-data:www-data /var/www/
-sudo systemctl restart apache2.service
 
 sudo systemctl unmask hostapd
 sudo systemctl enable hostapd
 sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
 sudo rfkill unblock wlan
+
+cp ../Config/config.php /var/www/html/owncloud
+cp ../Config/dhcpcd.conf /etc/dhcpcd.conf
+sudo cp ../Config/dnsmasq.conf /etc/dnsmasq.conf
+cp ../Config/hostapd.conf /etc/hostapd/hostapd.conf
+cp .bashrc /home/pi
+
+sudo reboot
