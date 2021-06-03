78a150f130d5c161aa777727eeb70bd93d06c4f8
Koen-Hertmans <61520912+Koen-Hertmans@users.noreply.github.com>
2021-06-03 17:10:15 +0200

    Update startupscript.sh

--- Files/Script/startupscript.sh
+++ Files/Script/startupscript.sh
@@ -1,31 +1,43 @@
 #!/bin/bash
 
+echo "Updating system..."
 sudo apt-get update
-for pack in cat `cat list2.txt`
-do sudo apt-get -y install $pack
-done
+sudo apt-get upgrade
 
+echo "Installing Dependencies"
+sudo apt-get install mosquitto mosquitto-clients
+sudo apt-get install mariadb-server-10.0
+
+echo "Installing Python libraries"
+sudo apt-get install python-mysql.connector
+sudo pip install paho-mqtt
+
+echo "Creating SQL database"
 sudo mysql -u root -p <<MYSQL_SCRIPT
-CREATE DATABASE VOGEL;
+CREATE DATABASE TRACKING;
 CREATE USER 'pi'@'localhost' IDENTIFIED BY 'rootroot';
-GRANT ALL PRIVILEGES ON VOGEL.* TO 'pi'@'localhost';
+GRANT ALL PRIVILEGES ON TRACKING.* TO 'pi'@'localhost';
 FLUSH PRIVILEGES;
+USE TRACKING;
+CREATE TABLE DEVICES(
+ID INT NOT NULL AUTO_INCREMENT,
+Device_id VARCHAR (100) NOT NULL,
+ROOM VARCHAR (100) NOT NULL,
+PRIMARY KEY (ID)
+);
+CREATE TABLE PEOPLECOUNT(
+Time DATETIME,
+ROOM1 int,
+ROOM2 int
+);
 MYSQL_SCRIPT
 
-wget https://download.owncloud.org/community/owncloud-complete-20200731.tar.bz2
-tar -xvf owncloud-complete-20200731.tar.bz2
-sudo mv owncloud /var/www/html/
-sudo chown -R www-data:www-data /var/www/
+echo "Installing Grafana"
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/$
+sudo apt-get update
+sudo apt-get install -y grafana
+sudo /bin/systemctl enable grafana-server
+sudo /bin/systemctl start grafana-server
 
-sudo systemctl unmask hostapd
-sudo systemctl enable hostapd
-sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
-sudo rfkill unblock wlan
 
-sudo cp ../Config/config.php /var/www/html/owncloud
-sudo cp ../Config/dhcpcd.conf /etc/dhcpcd.conf
-sudo cp ../Config/dnsmasq.conf /etc/dnsmasq.conf
-sudo cp ../Config/hostapd.conf /etc/hostapd/hostapd.conf
-cp .bashrc /home/pi
-
-sudo reboot
